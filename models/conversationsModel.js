0dadeeb93a033b09474534e390b2f0c2f205409a
Tchelet-Perlmutter <tchelet.perlmutter@gmail.com>
2021-11-07 19:33:11 +0200

    Added validator to 'conversationalistsIndexesArr' property of conversation schema

--- models/conversationsModel.js
+++ models/conversationsModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const User = require("./usersModel");
 
 // conversation schema.
 const conversationSchema = new mongoose.Schema({
@@ -10,8 +11,48 @@ const conversationSchema = new mongoose.Schema({
   conversationalistsIndexesArr: {
     type: Array,
     required: [true, "A conversation should have conversationalists"],
+    validate: {
+      validator: async (conversationalistsArr) => {
+        if (conversationalistsArr.length > 2) {
+          return false;
+        }
+        for (let i = 0; i <= 1; i++) {
+          if ((await isValidId(conversationalistsArr[i])) == false) {
+            return false;
+          }
+        }
+        return true;
+      },
+      message:
+        "'conversationalistsIndexesArr' property have to contain two valid mongoDB _IDs of other users",
+    },
   },
 });
+
+async function isValidId(fromVal) {
+  let toReturn = "";
+  console.log(` ----> 34 toReturn: ${toReturn}, fromVal: ${fromVal}`);
+  if (fromVal.length == 24) {
+    console.log(` ----> 35 toReturn: ${toReturn}, fromVal: ${fromVal}`);
+    toReturn = await User.findById(mongoose.Types.ObjectId(fromVal))
+      .then((doc) => {
+        if (doc == null) {
+          console.log(`---> null doc from isValidId: ${doc}`);
+          return false;
+        } else {
+          return true;
+        }
+      })
+      .catch((err) => {
+        console.log(`---> ERROR from the validator: ${err}`);
+      });
+  } else {
+    console.log(` ----> 49 toReturn: ${toReturn}, fromVal: ${fromVal}`);
+    toReturn = false;
+  }
+  console.log(`---> toReturn: ${toReturn}`);
+  return toReturn;
+}
 
 // Conversation model
 const Conversation = mongoose.model("conversations", conversationSchema);
